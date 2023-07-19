cca158f5426add8da8f56951fe197d6b463a111d
James-Ukoli <thejamesukoli@gmail.com>
2023-07-19 15:12:50 -0500

    Post model draft 1 completed

--- src/main/java/com/fantasypop/api/model/Post.java
+++ src/main/java/com/fantasypop/api/model/Post.java
@@ -18,8 +18,89 @@ public class Post {
     private String comments;
 
     //Constructor
+    public Post(int id, String username, String content, int voteRatio, int totalVotes, LocalDateTime timestamp, Map<String, String> keyPlayers, String sport, int flagCount, String topic, String comments) {
+        this.id = id;
+        this.username = username;
+        this.content = content;
+        this.voteRatio = voteRatio;
+        this.totalVotes = totalVotes;
+        this.timestamp = timestamp;
+        this.keyPlayers = keyPlayers;
+        this.sport = sport;
+        this.flagCount = flagCount;
+        this.topic = topic;
+        this.comments = comments;
+    }
 
-    //Methods
+    //Methods get set
+    public int getID(){
+        return id;
+    }
+    public void setID(int id) { this.id = id;}
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public int getVoteRatio(){
+        return voteRatio;
+    }
+    public void setVoteRatio(int voteRatio){
+        this.voteRatio = voteRatio;
+    }
+    public int getTotalVotes(){
+        return totalVotes;
+    }
+    public void setTotalVotes(int totalVotes) {
+        this.totalVotes = totalVotes;
+    }
+    public LocalDateTime getTimestamp(){
+        return timestamp;
+    }
+    public void setTimeStamp(LocalDateTime timestamp){
+        this.timestamp = timestamp;
+    }
+    public Map<String, String> getKeyPlayers(){
+        return keyPlayers;
+    }
+    public void setKeyPlayers(Map<String, String> keyPlayers) {
+        this.keyPlayers = keyPlayers;
+    }
+    public String getSport(){
+        return sport;
+    }
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+    public int getFlagCount(){
+        return flagCount;
+    }
+    public void setFlagCount(int flagCount){
+        this.flagCount = flagCount;
+    }
+    public String getTopic(){
+        return topic;
+    }
+    public void setTopic(String topic){
+        this.topic = topic;
+    }
+    public String getComments(){
+        return comments;
+    }
+    public void setComments(){
+        this.comments = comments;
+    }
+
+
+
+
 
 
 
--- src/main/java/com/fantasypop/api/model/User.java
+++ src/main/java/com/fantasypop/api/model/User.java
@@ -35,71 +35,54 @@ public class User {
     public int getID() {
         return id;
     }
-
     public void setID(int id) {
         this.id = id;
     }
-
     public String getFirstname() {
         return firstname;
     }
-
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
-
     public String getLastname() {
         return lastname;
     }
-
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
-
     public String getUsername(){
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getPassword(){
         return password;
     }
-
     public void setPassword(String password){
         this.password = password;
     }
-
     public LocalDate getBirthday(){
         return birthday;
     }
     public void setBirthday(LocalDate birthday) {
         this.birthday = birthday;
     }
-
     public String getProfilePic(){
         return profilePic;
     }
-
     public void setProfilePic(String profilePic){
         this.profilePic = profilePic;
     }
-
     public Map<String, String> getSocialMediaLinks(){
         return socialMediaLinks;
     }
-
     public void setSocialMediaLinks(Map<String,String> socialMediaLinks) {
         this.socialMediaLinks = socialMediaLinks;
     }
