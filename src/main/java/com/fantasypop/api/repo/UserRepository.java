2cbde02113e553af97aceaf3da303ba0f28394fa
James-Ukoli <thejamesukoli@gmail.com>
2023-07-25 11:14:25 -0500

    Repository file JPA added

---
+++ src/main/java/com/fantasypop/api/repo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.fantasypop.api.repo;
+
+import com.fantasypop.api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
